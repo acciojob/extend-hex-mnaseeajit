c006e75dd0dfafcb4f4114415b18cf3f81061a98
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-07 18:40:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const extendHex = (shortHex) => {
   // write your code here
-	if(shortHex.charAt(0) == #){
+	
 	return shortHex.charAt(0) + shortHex.charAt(1) + shortHex.charAt(1) +
            shortHex.charAt(2) + shortHex.charAt(2) +
            shortHex.charAt(3) + shortHex.charAt(3) ;
-	}
-    
-	return	shortHex.charAt(1) + shortHex.charAt(1) +
-           shortHex.charAt(2) + shortHex.charAt(2) +
-           shortHex.charAt(3) + shortHex.charAt(3) ;
+	
 
 };
 
