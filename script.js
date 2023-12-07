f14c0fe79e0c7037660c88aa2e862e2aad521f17
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-07 18:25:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const extendHex = (shortHex) => {
   // write your code here
+	if(shortHex.charAt(0) == #){
 	return shortHex.charAt(0) + shortHex.charAt(1) + shortHex.charAt(1) +
            shortHex.charAt(2) + shortHex.charAt(2) +
            shortHex.charAt(3) + shortHex.charAt(3) ;
-	
+	}
     
+	return	shortHex.charAt(1) + shortHex.charAt(1) +
+           shortHex.charAt(2) + shortHex.charAt(2) +
+           shortHex.charAt(3) + shortHex.charAt(3) ;
+
 };
 
-// Do not change the code below.
+// Do not change the code below.          
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
