4b5c4269521e716610eeca72ca22c41edce68b83
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-07 18:19:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const extendHex = (shortHex) => {
   // write your code here
+	return shortHex.charAt(0) + shortHex.charAt(1) + shortHex.charAt(1) +
+           shortHex.charAt(2) + shortHex.charAt(2) +
+           shortHex.charAt(3) + shortHex.charAt(3) ;
+	
+    
 };
 
 // Do not change the code below.
